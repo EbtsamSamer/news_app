89ea780e53db10614a58cfad7456f3a8e283742a
Ebtsam Samer <ebtsamsamer53@gmail.com>
2025-08-14 11:02:20 +0300

    Init App

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app/home/home_screen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      home:HomeScreen() ,
+      routes: {
+        HomeScreen.routeName:(_)=>HomeScreen(),
+      },
+      initialRoute:HomeScreen.routeName ,
+    );
+  }
+}
